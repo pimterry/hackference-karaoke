8c4f2e5d29a85b9c1b0dde0416a7e41996e3329b
Callum Hopkins <callum.e.hopkins@gmail.com>
2015-10-03 18:25:44 +0100

    adding lyrics grabber

--- public/cals-scripts.js
+++ public/cals-scripts.js
@@ -1,3 +1,6 @@
+  var getLyrics = false;
+  var trackInfo = {};
+
   DZ.init({
     appId  : '165345',
     channelUrl : 'http://localhost:3000/channel.html',
@@ -5,11 +8,52 @@
       container: 'musicPlayer',
       width : 800,
       height : 300,
-      onload : function(){
-          //DZ.player.playTracks([3135556, 1152226]);
-      }
+      onload : onMusicPlayerLoaded
     }
   });
+
+  function onMusicPlayerLoaded()
+  {
+
+    /*DZ.Event.subscribe('player_play', function(arg){
+      DZ.Event.subscribe('player_position', function(arg){
+        if(arg[0] > 0 && arg[0] < 1){
+          console.log()
+        }
+        console.log(arg);
+        //event_listener_append('position', arg[0], arg[1]);
+        //$(&quot;#slider_seek&quot;).find('.bar').css('width', (100*arg[0]/arg[1]) + '%');
+      });
+    });*/
+
+    DZ.Event.subscribe('player_buffering', function(){
+      if(!getLyrics){
+        console.log(trackInfo);
+
+        var posTitle = trackInfo.title.indexOf(' (Originally Performed by ');
+        var strLngh = trackInfo.length;
+
+        var trackName = trackInfo.title.substring(0,posTitle);
+        var trackArtists = trackInfo.title.substring(posTitle, strLngh);
+
+        console.log(trackName);
+        console.log(strLngh);
+
+        /*$.ajax({
+          url :lyricsApiUrl+"matcher.track.get?q_artist="++"&q_track="+,
+          data : {},
+          jsonp : 'callback',
+          success : function(albumData) {
+          }
+        });*/
+      }
+    });
+  }
+
+  function getTheLyrics()
+  {
+    //matcher.track.get?q_artist=eminem&q_track=lose%20yourself%20(soundtrack)
+  }
 
 	function httpGetAlbumTracks(apiUrl)
 	{
@@ -35,8 +79,8 @@
                   albumTracks = trackData;
                   for (var track of albumTracks.data) {
 
-                    var shitHead = '"'+track.id+'"'
-                    listHTML.innerHTML += "<li><a href='#"+track.id+"' onclick='loadSpotifySong("+shitHead+"); return false;'>"+track.title+"</a></li>";
+                      var trackData = '"'+encodeURIComponent(JSON.stringify(track))+'"'
+                      listHTML.innerHTML += "<li><a href='#"+track.id+"' onclick='loadSpotifySong("+trackData+"); return false;'>"+track.title+"</a></li>";
                       document.getElementById('tracks').appendChild(listHTML);
                       songLinks = document.querySelectorAll('a'); 
                   }
@@ -53,13 +97,15 @@
     };
 	}
 
-	function loadSpotifySong(songID)
+	function loadSpotifySong(track)
 	{
-    DZ.player.playTracks([songID]);
+    trackInfo = JSON.parse(decodeURIComponent(track));
+    DZ.player.playTracks([trackInfo.id]);
 	}
 
 
 	var apiUrl = 'http://api.deezer.com';
+  var lyricsApiUrl = 'http://api.musixmatch.com/ws/1.1/';
   var artistId = '321401';
 	var albumTracks = songlinks = '';
 
